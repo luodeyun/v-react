5a033153e8e9915b728e10bb15085d2581e8d2a7
ldy <807799686@qq.com>
2020-11-28 19:55:08 +0800

    react cli01

---
+++ react-cli01/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import React from 'react'
+class App extends React.Component {
+  render() {
+    return <h1 className = 'demo' >
+      这是我第一次使用脚手架 
+      </h1>
+  }
+}
+export default App;
