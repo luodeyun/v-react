add23a45ae98d31f17c0a6b6670c68d4806ab80e
ldy <807799686@qq.com>
2020-11-28 20:43:07 +0800

    react03 cli

--- react-cli01/src/App.js
+++ react-cli01/src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
 import React from 'react'
+import Hello from './Hello.js'
+import './App.css';
 class App extends React.Component {
   render() {
-    return <h1 className = 'demo' >
-      这是我第一次使用脚手架 
+    return (
+    <div>
+      <h1 className = 'demo'>
+           我是App
       </h1>
+      <Hello/>
+      </div>)
   }
 }
 export default App;
---
+++ react-cli01/src/Hello.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export default class Hello extends React.Component{
+    render(){
+        return(
+            <div>我是hello组件</div>
+        )
+    }
+}
